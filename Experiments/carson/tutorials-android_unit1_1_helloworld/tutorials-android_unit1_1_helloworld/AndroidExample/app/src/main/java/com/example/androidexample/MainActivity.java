2b8ad6482754684874459823324c881c0a196774
Carson Ferree <ceferree@iastate.edu>
2025-02-07 01:20:13 -0600

    Changed Hello World message and added secondary message to app

--- Experiments/carson/tutorials-android_unit1_1_helloworld/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/carson/tutorials-android_unit1_1_helloworld/tutorials-android_unit1_1_helloworld/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
 
+    private TextView secondMessageText; // Second message textview for experimentation
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         /* initialize UI elements */
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        messageText.setText("Hello World");
+        messageText.setText("Hello Coms 309!");
+        messageText.setTextColor(Color.BLUE);
+        messageText.setTextSize(12);
+
+        secondMessageText = findViewById(R.id.secondary_txt);
+        secondMessageText.setText("This is a Hello World App.");
+        secondMessageText.setX(240);
     }
 }
