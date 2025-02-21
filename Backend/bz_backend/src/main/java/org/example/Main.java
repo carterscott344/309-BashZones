ad563ce9733c9546c92bb238b8b3dfb414a2b239
Joseph Sheets <JSheets1@IAState.edu>
2025-02-21 15:21:31 -0600

    Test

--- Backend/bz_backend/src/main/java/org/example/Main.java
+++ Backend/bz_backend/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class  {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class, args);
+    }
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello, World!";
     }
 }
