5f913038d494d59d4fa70d86edbe6486c85e1133
Joseph Sheets <JSheets1@IAState.edu>
2025-02-23 21:17:30 -0600

    added models

--- Backend/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/repository/userRepository.java
+++ Backend/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/repository/userRepository.java
@@ -1,4 +1,10 @@
 package coms309.repository;
 
-public class userRepository {
+import coms309.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
