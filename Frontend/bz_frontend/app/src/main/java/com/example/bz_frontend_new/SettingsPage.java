bd5e9d2f07da7ce071242ca1d58e5f49c9635af7
Carson Ferree <ceferree@iastate.edu>
2025-02-24 23:25:35 -0600

    Added intent to most buttons to launch their respective activities (missing matchmaking as that is on backlog)

--- Frontend/bz_frontend/app/src/main/java/com/example/bz_frontend_new/SettingsPage.java
+++ Frontend/bz_frontend/app/src/main/java/com/example/bz_frontend_new/SettingsPage.java
@@ -1,5 +1,6 @@
 package com.example.bz_frontend_new;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -13,12 +14,9 @@ public class SettingsPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Set horizontal orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
