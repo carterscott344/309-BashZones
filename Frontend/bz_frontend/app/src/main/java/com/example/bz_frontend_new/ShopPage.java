1b45be3c8f42083d1d6a7ed2dd4ded9b69ac7415
Carson Ferree <ceferree@iastate.edu>
2025-02-28 22:14:30 -0600

    Added proper tabs using fragments to shop page

--- Frontend/bz_frontend/app/src/main/java/com/example/bz_frontend_new/ShopPage.java
+++ Frontend/bz_frontend/app/src/main/java/com/example/bz_frontend_new/ShopPage.java
@@ -11,11 +11,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class ShopPage extends AppCompatActivity {
 
+    // Fields for ViewPager usage
+    ViewPager2 viewPager2;
+    ShopViewPagerAdapter shopViewPagerAdapter;
+
     // Important button fields
     Button return_button;
+    Button skins_button;
+    Button banners_button;
+    Button tags_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +33,57 @@ public class ShopPage extends AppCompatActivity {
         // Set horizontal orientation
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        // Initialize ViewPager fields
+        viewPager2 = findViewById(R.id.shop_viewpager);
+        shopViewPagerAdapter = new ShopViewPagerAdapter(this);
+        viewPager2.setAdapter(shopViewPagerAdapter);
+
         // Initialize important button fields
         return_button = findViewById(R.id.shop_return_button);
+        skins_button = findViewById(R.id.shop_skins_button);
+        banners_button = findViewById(R.id.shop_banners_button);
+        tags_button = findViewById(R.id.shop_tags_button);
 
         // Set important button onClick listeners
         return_button.setOnClickListener(this::returnToGeneral);
+        skins_button.setOnClickListener(this::onSkinsClicked);
+        banners_button.setOnClickListener(this::onBannersClicked);
+        tags_button.setOnClickListener(this::onTagsClicked);
+
+        // Because skins fragment is the default, skins button will be silver on activity creation
+        skins_button.setBackgroundResource(R.color.silver);
     }
 
     // Important onClick methods
     public void returnToGeneral(View v) {
         Intent i = new Intent(this, GeneralPage.class);
         startActivity(i);
+    }
+
+    public void onSkinsClicked(View v) {
+        // Set color to gray to indicate it has been clicked, set other buttons to white
+        skins_button.setBackgroundResource(R.color.silver);
+        banners_button.setBackgroundResource(R.color.white);
+        tags_button.setBackgroundResource(R.color.white);
+
+        // Set ViewPager fragment to open specific shop tab
+        viewPager2.setCurrentItem(0);
+    }
+
+    public void onBannersClicked(View v) {
+        skins_button.setBackgroundResource(R.color.white);
+        banners_button.setBackgroundResource(R.color.silver);
+        tags_button.setBackgroundResource(R.color.white);
+
+        viewPager2.setCurrentItem(1);
+    }
+
+    public void onTagsClicked(View v) {
+        skins_button.setBackgroundResource(R.color.white);
+        banners_button.setBackgroundResource(R.color.white);
+        tags_button.setBackgroundResource(R.color.silver);
+
+        viewPager2.setCurrentItem(2);
     }
 
     // Makes an individual shop item from an array of item attributes
---
+++ Frontend/bz_frontend/app/src/main/java/com/example/bz_frontend_new/ShopViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.bz_frontend_new;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.bz_frontend_new.shop_fragments.BannersFragment;
+import com.example.bz_frontend_new.shop_fragments.SkinsFragment;
+import com.example.bz_frontend_new.shop_fragments.TagsFragment;
+
+public class ShopViewPagerAdapter extends FragmentStateAdapter {
+    public ShopViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new SkinsFragment();
+            case 1:
+                return new BannersFragment();
+            case 2:
+                return new TagsFragment();
+            default:
+                return new SkinsFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
