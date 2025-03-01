d0e1fe732283d1ce473261e6c9a080606912dc04
Joseph Sheets <JSheets1@IAState.edu>
2025-03-01 15:11:28 -0600

    fixed post method

---
+++ Backend/ZoneZoneApplicationBackend/src/main/java/ZoneZone/com/ApplicationStart.java
@@ -0,0 +1,11 @@
+package ZoneZone.com; // Adjust the package name as needed
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
