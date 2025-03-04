9bbfb19bc527671fb6a39ebc10f0b8d959aa0b5e
Carson Ferree <ceferree@iastate.edu>
2025-03-04 16:59:18 -0600

    Created fragments and viewpager adapter for user inventory page

---
+++ Frontend/bz_frontend/app/src/main/java/com/example/bz_frontend_new/InvViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.bz_frontend_new;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.bz_frontend_new.invfragments.InvBannersFragment;
+import com.example.bz_frontend_new.invfragments.InvHatsFragment;
+import com.example.bz_frontend_new.invfragments.InvTagsFragment;
+
+public class InvViewPagerAdapter extends FragmentStateAdapter {
+    public InvViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new InvHatsFragment();
+            case 1:
+                return new InvBannersFragment();
+            case 2:
+                return new InvTagsFragment();
+            default:
+                return new InvHatsFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
