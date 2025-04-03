547e79d80c98ebd087409ee45a90f74aa28cfc4b
Carson Ferree <ceferree@iastate.edu>
2025-04-03 03:24:05 -0500

    Created true player class, will be source of rendering in near future

---
+++ Frontend/bz_frontend/app/src/main/java/com/example/bz_frontend_new/Player.java
@@ -0,0 +1,24 @@
+package com.example.bz_frontend_new;
+
+import android.content.Context;
+import android.graphics.Canvas;
+
+public class Player {
+    private double posX;
+    private double posY;
+
+    public Player(Context context, double posX, double posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    // Updating game logic
+    public void update(Joystick leftJoystick) {
+
+    }
+
+    // Rendering method
+    public void render(Canvas canvas) {
+
+    }
+}
