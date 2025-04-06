f8301609e833b0d7d67405d72509ee2511a5001f
Joseph Sheets <JSheets1@IAState.edu>
2025-04-06 17:53:40 -0500

    added player position updating and, add match, load players, and start match functionality

--- gameServer/ZoneZoneGameServer/src/main/java/com/zonezone/backend/gameHandler/MatchAddPayload.java
+++ gameServer/ZoneZoneGameServer/src/main/java/com/zonezone/backend/gameHandler/MatchAddPayload.java
@@ -2,7 +2,7 @@ package com.zonezone.backend.gameHandler;
 
 import java.util.List;
 
-public class MatchStartPayloadDTO {
+public class MatchAddPayload {
     public String matchID;
     public List<String> teamA;
     public List<String> teamB;
