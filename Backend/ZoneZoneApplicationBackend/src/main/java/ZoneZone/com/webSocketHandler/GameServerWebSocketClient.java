778dc6979f26a1a43c3c91731b1b3330a0b8634c
Joseph Sheets <JSheets1@IAState.edu>
2025-04-06 22:09:13 -0500

    fixed backend

--- Backend/ZoneZoneApplicationBackend/src/main/java/ZoneZone/com/webSocketHandler/GameServerWebSocketClient.java
+++ Backend/ZoneZoneApplicationBackend/src/main/java/ZoneZone/com/webSocketHandler/GameServerWebSocketClient.java
@@ -10,7 +10,7 @@ import java.net.URI;
 
 public class GameServerWebSocketClient {
 
-    private static final String SERVER_URI = "ws:// /ws/connectToServer";
+    private static final String SERVER_URI = "ws://localhost:80/ws/connectToServer";
     private static final Gson gson = new Gson();
 
     public static void sendMatchPayload(WebSocketMessageDTO payload) {
